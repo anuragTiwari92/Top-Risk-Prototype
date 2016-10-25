8f578ed2b193a8aead9a0ddb1cadc728d12f0183
user122182 <user122182@74-80-239-248.ic2net.net>
2016-10-25 14:04:17 -0700

    Unit 3 done (-quiz)

--- Top Risk Prototype/Chapter3Controller.swift
+++ Top Risk Prototype/Chapter3Controller.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class Chapter3Controller: UIViewController{
     
-    @IBOutlet var Chapter3Video: UIWebView!
+
+    @IBOutlet weak var Chapter3Video: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let youtbelink3 = "https://www.youtube.com/embed/sWCCYhvZHr0"
+        let youtbelink3 = "https://www.youtube.com/embed/SCGopFuIzLI"
         Chapter3Video.allowsInlineMediaPlayback = true;
-        Chapter3Video.loadHTMLString("<iframe width=\"\(Chapter3Video.frame.width)\" height=\"\(Chapter3Video.frame.height)\" src=\"\(youtbelink3)?&playsinline=1\" frameborder=\"0\" allowfullscreen></iframe>", baseURL: nil);
+        Chapter3Video.loadHTMLString("<html><head><title>.</title><style>body,html,iframe{margin:0;padding:0;}</style></head><body><iframe width=\"\(Chapter3Video.frame.width)\" height=\"\(Chapter3Video.frame.height)\" src=\"\(youtbelink3)?&playsinline=1\" frameborder=\"0\" allowfullscreen></iframe></body></html>", baseURL: nil);
         
     }
     
