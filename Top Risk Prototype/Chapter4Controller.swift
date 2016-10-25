e5dad5475e1d5e559ad60ca1b6c9db3f405b98db
user122182 <user122182@74-80-239-248.ic2net.net>
2016-10-25 12:36:34 -0700

    Finished unit 4 (without quiz)

---
+++ Top Risk Prototype/Chapter4Controller.swift
@@ -0,0 +1,35 @@
+//
+//  Chapter4Controller.swift
+//  Top Risk Prototype
+//
+//  Created by user122182 on 10/25/16.
+//  Copyright © 2016 user122171. All rights reserved.
+//
+
+import UIKit
+
+class Chapter4Controller: UIViewController{
+
+
+    @IBOutlet weak var Chapter4Video: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let youtbelink4 = "https://www.youtube.com/embed/sWCCYhvZHr0"
+        Chapter4Video.allowsInlineMediaPlayback = true;
+        Chapter4Video.loadHTMLString("<iframe width=\"\(Chapter4Video.frame.width)\" height=\"\(Chapter4Video.frame.height)\" src=\"\(youtbelink4)?&playsinline=1\" frameborder=\"0\" allowfullscreen></iframe>", baseURL: nil);
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        Chapter4Video.reload()
+        super.viewWillDisappear(animated)
+        
+    }
+}
