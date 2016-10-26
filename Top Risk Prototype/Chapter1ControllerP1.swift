7407c9c1a03e36cd2db7d83d75006398b5a9f3eb
anuragTiwari92 <anuragti@usc.edu>
2016-10-26 14:43:23 -0700

    popup Label in chapter 1 what is go grrrls

--- Top Risk Prototype/Chapter1ControllerP1.swift
+++ Top Risk Prototype/Chapter1ControllerP1.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class Chapter1ControllerP1: UIViewController{
     
+    @IBOutlet var whatIS: UIButton!
+    
+    @IBOutlet var puzzleButton: UIButton!
+    
+    @IBOutlet var whatsInApp: UIButton!
+    
+    @IBOutlet var WhatISLabel: UILabel!
+    
+    @IBAction func popupLabel(_ sender: AnyObject) {
+        WhatISLabel.isHidden = false
+    }
+    
+    
+    @IBAction func touchupLabel(_ sender: AnyObject) {
+        WhatISLabel.isHidden = true
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        whatIS.layer.cornerRadius = 5
+        whatIS.layer.borderWidth = 1
+        whatIS.layer.borderColor = UIColor.black.cgColor
         
+        puzzleButton.layer.cornerRadius = 5
+        puzzleButton.layer.borderColor = UIColor.black.cgColor
+        puzzleButton.layer.borderWidth = 1
+        
+        whatsInApp.layer.cornerRadius = 5
+        whatsInApp.layer.borderWidth = 1
+        whatsInApp.layer.borderColor = UIColor.black.cgColor
+        
+        WhatISLabel.isHidden = true
     }
     
     override func didReceiveMemoryWarning() {
