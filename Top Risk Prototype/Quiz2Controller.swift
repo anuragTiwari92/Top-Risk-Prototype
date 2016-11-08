3f6550e45fa52e25773faa4777f69e948c69917c
anuragTiwari92 <anuragti@usc.edu>
2016-11-07 18:11:27 -0800

    Quiz 2 3 4 7 checked for errors

---
+++ Top Risk Prototype/Quiz2Controller.swift
@@ -0,0 +1,82 @@
+//
+//  Quiz2Controller.swift
+//  Top Risk Prototype
+//
+//  Created by AnuragTiwari on 11/7/16.
+//  Copyright © 2016 SkylerLecroy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Quiz2Controller: UIViewController {
+    
+    @IBOutlet var Buttons: [UIButton]!
+    @IBOutlet var QLabel: UILabel!
+    
+    @IBOutlet var EndofQuizMsg: UITextView!
+    
+    var Questions = [Question]()
+    var QNumber = Int()
+    var AnswerNumber = Int()
+    var Male:Int=0
+    var Female:Int=0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        Questions = [Question(Question: "Is the principal of your school?", Answers: ["Male","Female"], Answer: 0),
+                     Question(Question: "Is the mayor of your city?", Answers: ["Male","Female"], Answer: 0),
+                     Question(Question: "Is the governor of your state?", Answers: ["Male","Female"], Answer: 0),
+                     Question(Question: "Is the Vice President of the United States?", Answers: ["Male","Female"], Answer: 0),
+                     Question(Question: "Is the President of the United States?", Answers: ["Male","Female"], Answer: 0),
+        ]
+        
+        PickQuestion()
+        
+        EndofQuizMsg.isHidden=true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func PickQuestion()
+    {
+        
+        
+        
+        if Questions.count > 0{
+            QNumber = 0
+            QLabel.text = Questions[QNumber].Question
+            AnswerNumber = Questions[QNumber].Answer
+            for i in 0..<Buttons.count{
+                Buttons[i].setTitle(Questions[QNumber].Answers[i], for: UIControlState.normal)
+            }
+            Questions.remove(at:QNumber)
+        }
+        else{
+            NSLog("Done with quiz!")
+            
+            EndofQuizMsg.isHidden=false
+            
+            EndofQuizMsg.text="The number of Males: \(Male)\nThe number of Females: \(Female)\n\n\n These are the number of Male and Female individuals you selected. \n Most girls will have only one or two circles around \"female\".  We have never had a female president or vice president in the United States.  Can you believe it?!!  Other countries have had women presidents and some states have women as governors, but most people who hold important political office are still men. \n\n What does this tell you about women’s roles in society today?  It tells us that progress is slow and we need to support more women who run for political office.\n\nAre there more women in office today than there were 100 years ago?  Absolutely. Things are changing for the better.  Maybe in another 100 years there will be about the same number of women and men in powerful leadership roles.  You can be a part of that movement.  Be a strong and intelligent woman, and support other women too!  "
+            
+            for i in 0..<Buttons.count{
+                Buttons[i].isHidden=true
+            }
+            QLabel.isHidden=true
+            
+        }
+    }
+    @IBAction func Btn1(_ sender: Any) {
+        Male += 1
+        PickQuestion()
+    }
+    
+    @IBAction func Btn2(_ sender: Any) {
+        Female += 1
+        PickQuestion()
+    }
+}
